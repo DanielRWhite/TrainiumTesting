7c9ad336b276eddfbb2ecd516857319ba5d9dbeb
DanielRWhite <32739484+DanielRWhite@users.noreply.github.com>
2023-03-27 15:57:10 +1100

    Update pt-lightning train script

--- train.py
+++ train.py
@@ -9,9 +9,7 @@ import torchvision
 import pytorch_lightning as pl
 import argparse
 
-from transformers import TrainingArguments
-from optimum.neuron import TrainiumTrainer as Trainer
-#import timm
+import timm
 
 class LightningPipeline(pl.LightningModule):
     def __init__(self, model):
@@ -19,8 +17,6 @@ class LightningPipeline(pl.LightningModule):
         self.model = model
 
     def training_step(self, batch, _):
-        # training_step defines the train loop.
-        # it is independent of forward
         inputs, positives, negatives = batch
         
         outputs = self.model(inputs)
@@ -30,12 +26,10 @@ class LightningPipeline(pl.LightningModule):
         loss = nn.functional.triplet_margin_loss(outputs, e_pos, e_neg, margin=0.8, p=2)
         
         # Logging to TensorBoard (if installed) by default
-        self.log("train_loss", loss)        
+        self.log("train_loss", loss, on_step = True, on_epoch = True, prog_bar = True, logger = True)        
         return loss
 
     def validation_step(self, batch, _):
-        # validation_step defines the val loop.
-        # it is independent of forward
         inputs, positives, negatives = batch
         
         outputs = self.model(inputs)
@@ -45,7 +39,7 @@ class LightningPipeline(pl.LightningModule):
         loss = nn.functional.triplet_margin_loss(outputs, e_pos, e_neg, margin=0.8, p=2)
 
         # Logging to TensorBoard (if installed) by default
-        self.log("val_loss", loss)
+        self.log("val_loss", loss, on_step = True, on_epoch = True, prog_bar = True, logger = True)        
         return loss
 
     def configure_optimizers(self):
@@ -54,7 +48,7 @@ class LightningPipeline(pl.LightningModule):
         
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument('--accelerator', type = str, choices = [ 'auto', 'cpu', 'gpu', 'tpu', 'ipu', 'hpu', 'mps' ], default = 'auto')
+    args.add_argument('--accelerator', type = str, default = 'auto')
     args.add_argument('--devices', default = 'auto')
     args.add_argument('--num_nodes', type = int, default = 1)
     args.add_argument('--max_epochs', type = int, default = 8)
@@ -63,22 +57,21 @@ if __name__ == "__main__":
     args.add_argument('--dataset', type = Path)
     hparams = args.parse_args()
     
-    #model = timm.create_model('maxvit_rmlp_pico_rw_256.sw_in1k', pretrained = False, num_classes = 0)
-    model = torchvision.models.efficientnet_v2_s(weights = None)
-    
+    model = timm.create_model('maxvit_rmlp_pico_rw_256.sw_in1k', pretrained = False, num_classes = 0)
+    #model = torchvision.models.efficientnet_v2_s(weights = None)
     pipeline = LightningPipeline(model)
     
-    #data_config = timm.data.resolve_model_data_config(model)
-    #transforms = timm.data.create_transform(**data_config, is_training = False)
+    data_config = timm.data.resolve_model_data_config(model)
+    transforms = timm.data.create_transform(**data_config, is_training = False)
     
-    pre_transforms = torchvision.transforms.Compose([
-        torchvision.transforms.Resize((384, 384)),
-        torchvision.transforms.CenterCrop((384, 384)),
-        torchvision.transforms.ToTensor()
-    ])
+    # pre_transforms = torchvision.transforms.Compose([
+    #     torchvision.transforms.Resize((384, 384)),
+    #     torchvision.transforms.CenterCrop((384, 384)),
+    #     torchvision.transforms.ToTensor()
+    # ])
     
-    train_dataset = DigiFaceDataset(hparams.dataset.joinpath("train"), pre_transforms = pre_transforms)
-    val_dataset = DigiFaceDataset(hparams.dataset.joinpath("val"), pre_transforms = pre_transforms)
+    train_dataset = DigiFaceDataset(hparams.dataset.joinpath("train"), pre_transforms = transforms, image_size = (384, 384))
+    val_dataset = DigiFaceDataset(hparams.dataset.joinpath("val"), pre_transforms = transforms, image_size = (384, 384))
 
     train_loader, val_loader = (
         utils.data.DataLoader(train_dataset, batch_size = hparams.batch_size, num_workers = hparams.num_workers),
@@ -95,7 +88,7 @@ if __name__ == "__main__":
         num_nodes = hparams.num_nodes,
         max_epochs = hparams.max_epochs,
         callbacks = [ cb_checkpoint ],
-        precision = 'bf16'
+        precision = 16
     )
     
     trainer.fit(
